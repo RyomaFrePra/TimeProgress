fa888e53accbe8b739124abf5e1d376c406ce3f7
ЯoomeR <ryomasenbra@gmail.com>
2023-04-18 10:13:11 +0900

    modify

--- scripts.js
+++ scripts.js
@@ -58,71 +58,101 @@ function getDaysOfYearProgress() {
   };
 }
 
-function formatHoursProgress(elapsed) {
-  const hours = Math.floor(elapsed / 60);
-  const minutes = elapsed % 60;
-  return `${hours}時間${minutes}分`;
+function formatHoursProgress(elapsed, remaining) {
+  const hoursElapsed = Math.floor(elapsed / 60);
+  const minutesElapsed = elapsed % 60;
+  const hoursRemaining = Math.floor(remaining / 60);
+  const minutesRemaining = remaining % 60;
+  return {
+    elapsed: `${hoursElapsed}時間${minutesElapsed}分`,
+    remaining: `${hoursRemaining}時間${minutesRemaining}分`
+  };
 }
 
-function formatDaysOfMonthProgress(elapsed) {
-  return `${elapsed}日`;
+function formatDaysOfMonthProgress(elapsed, remaining) {
+  return {
+    elapsed: `${elapsed}日`,
+    remaining: `${remaining}日`
+  };
 }
 
-function formatDaysOfYearProgress(elapsed) {
+function formatDaysOfYearProgress(elapsed, remaining) {
   const now = getCurrentTime();
   const year = now.getFullYear();
-  const month = now.getMonth();
 
   const startOfYear = new Date(year, 0, 1);
+  const endOfYear = new Date(year, 11, 31);
   const elapsedDate = new Date(startOfYear.getTime() + elapsed * 24 * 60 * 60 * 1000);
+
+  const remainingDaysInYear = (endOfYear - elapsedDate) / (1000 * 60 * 60 * 24);
+  const remainingDate = new Date(elapsedDate.getTime() + remainingDaysInYear * 24 * 60 * 60 * 1000);
+
   const elapsedMonth = elapsedDate.getMonth();
   const elapsedDay = elapsedDate.getDate() - 1;
 
-  return `${elapsedMonth}か月${elapsedDay}日`;
+  const daysInElapsedMonth = new Date(year, elapsedMonth + 1, 0).getDate();
+  const remainingMonth = Math.floor(remaining / daysInElapsedMonth);
+  const remainingDay = remaining % daysInElapsedMonth;
+
+  return {
+    elapsed: `${elapsedMonth}か月${elapsedDay}日`,
+    remaining: `${remainingMonth}か月${remainingDay}日`
+  };
 }
 
-function createProgressChart(elementId, progress, formatElapsed, elapsedElement, remainingElement, percentageElement) {
+
+
+function createProgressChart(elementId, progress, formatProgress, elapsedElement, remainingElement, percentageElement) {
   const ctx = document.getElementById(elementId).getContext('2d');
-  const chart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ['経過', '残り'],
-      datasets: [{
-        data: [progress.elapsed, progress.remaining],
-        backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 255, 255, 0.2)'],
-        borderColor: ['rgba(75, 192, 192, 1)', 'rgba(0, 0, 0, 1)'],
-        borderWidth: 1
-      }]
+const chart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['経過', '残り'],
+    datasets: [{
+      data: [progress.elapsed, progress.remaining],
+      backgroundColor: ['rgba(75, 192, 192, 1)', 'rgba(230, 230, 230, 1)'],
+      borderColor: ['rgba(75, 192, 192, 1)', 'rgba(230, 230, 230, 1)'],
+      borderWidth: 2,
+      hoverBackgroundColor: ['rgba(75, 192, 192, 0.8)', 'rgba(230, 230, 230, 0.8)'],
+      hoverBorderColor: ['rgba(75, 192, 192, 1)', 'rgba(230, 230, 230, 1)'],
+      hoverBorderWidth: 2
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      display: false
     },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      tooltips: {
-        enabled: false
-      },
-      plugins: {
-        datalabels: {
-          color: 'black',
-          align: 'center',
-          formatter: function(value, context) {
-            const total = context.dataset.data.reduce((a, b) => a + b);
-            const percentage = Math.round((value / total) * 100);
-            return `${value} (${percentage}%)`;
-          }
+    tooltips: {
+      enabled: false
+    },
+    cutoutPercentage: 50, // 中央の穴の大きさを変更
+    animation: {
+      animateRotate: true,
+      animateScale: true
+    },
+    plugins: {
+      datalabels: {
+        color: 'black',
+        align: 'center',
+        formatter: function (value, context) {
+          const total = context.dataset.data.reduce((a, b) => a + b);
+          const percentage = Math.round((value / total) * 100);
+          return `${value} (${percentage}%)`;
         }
       }
     }
-  });
+  }
+});
 
-  const formattedElapsed = formatElapsed(progress.elapsed);
+
+  const formattedProgress = formatProgress(progress.elapsed, progress.remaining);
   const total = progress.elapsed + progress.remaining;
   const percentage = Math.round((progress.elapsed / total) * 100);
 
-  elapsedElement.innerText = `経過: ${formattedElapsed}`;
-  remainingElement.innerText = `残り: ${progress.remaining}`;
+  elapsedElement.innerText = `経過: ${formattedProgress.elapsed}`;
+  remainingElement.innerText = `残り: ${formattedProgress.remaining}`;
   percentageElement.innerText = `割合: ${percentage}%`;
 }
 
